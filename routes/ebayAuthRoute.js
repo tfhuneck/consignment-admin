92116d9afa9514008db49b3a1f5c5eb0317354ba
Timothy Huneck <timothyhuneck@Timothys-MBP.lan>
2023-11-16 16:56:29 -0800

    repo clean up

--- routes/ebayAuthRoute.js
+++
@@ -1,8 +0,0 @@
-const express   = require('express')
-const router    = express.Router();
-const userToken = require('../services/ebayAuth')
-
-router.route('/')
-    .get(userToken)
-
-module.exports = router;
--- routes/ebayRefreshTokenRoute.js
+++
@@ -1,8 +0,0 @@
-const express   = require('express');
-const router    = express.Router();
-const getToken  = require('../services/ebayConvertRefreshToken');
-
-router.route('/')
-    .get(getToken)
-
-module.exports = router; 
